085ebf7f9514c5810e9ee2494c690458435cf913
Jason Crowe (Mozilla) <jcrowe@mozilla.com>
2017-06-14 17:42:47 -0400

    Making it work on Amazon and Ubuntu

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,8 +40,8 @@ class duo_unix (
       $gpg_file    = '/etc/pki/rpm-gpg/RPM-GPG-KEY-DUO'
 
       $pam_file = $::operatingsystemrelease ? {
-        /^5/ => '/etc/pam.d/system-auth',
-        /^(6|7|2014|2017)/ => '/etc/pam.d/password-auth'
+        /^(5|2017)/ => '/etc/pam.d/system-auth',
+        /^(6|7|2014)/ => '/etc/pam.d/password-auth'
       }
 
       $pam_module  = $::architecture ? {
--- manifests/pam.pp
+++ manifests/pam.pp
@@ -24,10 +24,53 @@ class duo_unix::pam inherits duo_unix {
       changes => [
         'set /files/etc/ssh/sshd_config/UsePAM yes',
         'set /files/etc/ssh/sshd_config/UseDNS no',
-        'set /files/etc/ssh/sshd_config/ChallengeResponseAuthentication yes'
-      ],
+        'set /files/etc/ssh/sshd_config/ChallengeResponseAuthentication yes',
+        'set /files/etc/ssh/sshd_config/PubkeyAuthentication yes',
+        'set /files/etc/ssh/sshd_config/PasswordAuthentication no',
+        'set /files/etc/ssh/sshd_config/AuthenticationMethods publickey,keyboard-interactive',
+        'set /files/etc/ssh/sshd_config/PermitRootLogin no'
+    ],
       require => Package[$duo_unix::duo_package],
       notify  => Service[$duo_unix::ssh_service];
+    }
+    case $::osfamily {
+      'RedHat': {
+        augeas { 'Duo Security SSH PAM Configuration' :
+          changes => [
+            'rm /files/etc/pam.d/sshd/*[type = "auth"][module = "password-auth"]',
+            'ins 100 after /files/etc/pam.d/sshd/*[type = "auth"][module = "pam_sepermit.so"]',
+            'set /files/etc/pam.d/sshd/100/type auth',
+            'set /files/etc/pam.d/sshd/100/control required',
+            "set /files/etc/pam.d/sshd/100/module ${duo_unix::pam_module}"
+        ],
+          require => Package[$duo_unix::duo_package],
+          onlyif => "match /files/etc/pam.d/sshd/*[module = '${duo_unix::pam_module}'] size == 0";
+        }
+      }
+      'Debian': {
+        augeas { 'Duo Security SSH PAM Configuration' :
+          changes => [
+            'ins 100 after /files/etc/pam.d/sshd/include[1]',
+            'set /files/etc/pam.d/sshd/100/type auth',
+            'set /files/etc/pam.d/sshd/100/control "[success=1 default=ignore]"',
+            "set /files/etc/pam.d/sshd/100/module ${duo_unix::pam_module}",
+            "ins 200 after /files/etc/pam.d/sshd/*[type = 'auth'][module = '${duo_unix::pam_module}']",
+            'set /files/etc/pam.d/sshd/200/type auth',
+            'set /files/etc/pam.d/sshd/200/control requisite',
+            'set /files/etc/pam.d/sshd/200/module pam_deny.so',
+            'ins 300 after /files/etc/pam.d/sshd/*[type = "auth"][module = "pam_deny.so"]',
+            'set /files/etc/pam.d/sshd/300/type auth',
+            'set /files/etc/pam.d/sshd/300/control required',
+            'set /files/etc/pam.d/sshd/300/module pam_permit.so',
+            'rm /files/etc/pam.d/sshd/include[1]'
+        ],
+          require => Package[$duo_unix::duo_package],
+          onlyif => "match /files/etc/pam.d/sshd/*[module = '${duo_unix::pam_module}'] size == 0";
+        }
+      }
+      default: {
+        fail("Module ${module_name} does not support ${::osfamily}")
+      }
     }
   }
 
